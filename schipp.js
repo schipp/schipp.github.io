846409102eea819a376b096e5d2c6da792b83b0e
schipp <26145768+schipp@users.noreply.github.com>
2024-01-20 16:01:55 +0100

    chatgpt cleanup

--- schipp.js
+++ schipp.js
@@ -1,61 +1,47 @@
-// populate the page from the data in json
-// first time writing javascript, probably a huge mess
+function createElement(type, classNames, text = '') {
+    const element = document.createElement(type);
+    if (classNames) element.classList.add(...classNames.split(' '));
+    if (text) element.textContent = text;
+    return element;
+}
 
-function generate_grid(papers, grid_type) {
-    var grid_container = document.createElement("div");
-    grid_container.classList.add("grid-container")
-    for (var id in papers) {
-        // grid container
-        var grid_item = document.createElement("div");
-        grid_item.classList.add("grid-item");
-        // preview image
-        var img = document.createElement("img");
-        img.classList.add("image");
-        img.src = papers[id]["preview_img"];
-        var overlay_image = document.createElement("div");
-        overlay_image.classList.add("overlay-image");
-        overlay_image.appendChild(img);
+function generateGrid(papers, gridType) {
+    const gridContainer = createElement('div', 'grid-container');
 
-        if (papers[id]["tag"] == "new") {
-            var h3 = document.createElement("h3");
-            h3.innerText = "NEW";
+    Object.entries(papers).forEach(([id, paper]) => {
+        const gridItem = createElement('div', 'grid-item');
+        const img = createElement('img', 'image');
+        img.src = paper.preview_img;
+
+        const overlayImage = createElement('div', 'overlay-image');
+        overlayImage.appendChild(img);
+
+        if (paper.tag === 'new') {
+            overlayImage.appendChild(createElement('h3', null, 'NEW'));
         }
 
-        if (papers[id]["tag"] == "new") {
-            overlay_image.appendChild(h3);
+        if (gridType === 'papers') {
+            overlayImage.appendChild(createElement('journal', null, `${paper.journal}, ${paper.year}`));
         }
 
-        //  journal + year overlay ontop of image
-        if (grid_type == "papers") {
-            var journal = document.createElement("journal");
-            journal.innerText = papers[id]["journal"] + ", " + papers[id]["year"];
-            overlay_image.appendChild(journal);
-        }
+        const title = createElement('div', 'text', paper.title);
+        const link = createElement('a', null);
+        link.href = paper.url;
+        link.append(overlayImage, title);
+        gridItem.appendChild(link);
 
-        // name of paper/repository
-        var title = document.createElement("div");
-        title.classList.add("text");
-        title.innerText = papers[id]["title"]
+        gridContainer.appendChild(gridItem);
+    });
 
-        // href so everythings is a link
-        var a = document.createElement("a");
-        a.href = papers[id]["url"];
-        a.appendChild(overlay_image);
-        a.appendChild(title);
-        grid_item.appendChild(a);
-
-        grid_container.appendChild(grid_item);
-    }
-    return grid_container
+    return gridContainer;
 }
 
 fetch("data.json")
     .then(response => response.json())
     .then(data => {
-        var papers = data["publications"];
-        var repositories = data["repositories"];
-        grid_container_papers = generate_grid(papers, "papers")
-        grid_container_github = generate_grid(repositories, "repositories")
-        document.getElementsByClassName("paper_grid")[0].appendChild(grid_container_papers)
-        document.getElementsByClassName("github_grid")[0].appendChild(grid_container_github)
-    })
+        const { publications, repositories } = data;
+        const papersGridContainer = generateGrid(publications, 'papers');
+        const githubGridContainer = generateGrid(repositories, 'repositories');
+        document.querySelector('.paper_grid').appendChild(papersGridContainer);
+        document.querySelector('.github_grid').appendChild(githubGridContainer);
+    });
